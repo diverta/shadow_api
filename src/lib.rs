84e21d37d8eaf90ce72b97420fe062793940b022
Artem <artem@diverta.co.jp>
2023-05-09 15:06:05 +0900

    Export ShadowError

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,6 @@ mod shadow_api;
 pub use crate::shadow_api::ShadowApi;
 pub use crate::shadow_api::ShadowJson;
 pub use crate::shadow_api::ShadowData;
+pub use crate::shadow_api::ShadowError;
 pub use crate::shadow_api::ShadowDataCursor;
 pub use crate::shadow_api::ShadowApiOptions;
